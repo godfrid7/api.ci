122a907d296d0fc417fd24c618b3ed4333b06804
Ilya <godfrid7@mail.ru>
2023-03-23 14:24:13 +0300

    mobilebankv1

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -13,7 +13,7 @@ class MobileBankApiTestV1 {
                 .baseUri("http://localhost:9999/api/v1")
                 // Выполняемые действия
                 .when()
-                .get("/demo/accounts")
+                .get("/demo/account")
                 // Проверки
                 .then()
                 .statusCode(200);
